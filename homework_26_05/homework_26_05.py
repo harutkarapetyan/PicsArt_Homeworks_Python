c52400796f90a92eeb278d77763fe9f04ee0c1ae
Harut Karapetyan <132202487+harutkarapetyan@users.noreply.github.com>
2023-06-27 12:50:13 +0400

    Add files via upload

---
+++ homework_26_05/homework_26_05.py
@@ -0,0 +1,38 @@
+import random
+# 1)
+# kg = float(input("kg = "))
+# funt = 2.2 * kg
+# print("funt = ",funt)
+
+# 2)
+# for i in range(50):
+#     x = random.randint(3,6)
+#     print(x,end=" ")
+
+# 3)
+# x = int(input("x = "))
+# y = int(input("y = "))
+# result = abs(x + y) / (x + y)
+# print(result)
+
+# 4)
+# year = int(input("Input years = "))
+# caunt = 0
+# for x in range(1600, year + 1):
+#     if (x % 4 == 0 and x % 100 != 0) or x % 400 == 0:
+#         caunt += 1
+# print("caunt =",caunt)  
+
+# 5)
+# ls = []
+# for x in range(1,10000):
+#     sum = 0
+#     for i in range(1, x // 2 +1):
+#         if x % i == 0:
+#             sum += i
+#     if sum == x:
+#        ls.append(x)
+#     #    print(x)
+
+# print("result =",ls)
+
